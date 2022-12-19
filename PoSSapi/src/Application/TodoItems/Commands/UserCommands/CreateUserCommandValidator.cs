6a6255acdf1f1dc45a90eb6ccd7c87018a8cf8c0
Arnasst <59619111+Arnasst@users.noreply.github.com>
2022-12-19 14:23:14 +0200

    Allowed older people

--- PoSSapi/src/Application/TodoItems/Commands/UserCommands/CreateUserCommandValidator.cs
+++ PoSSapi/src/Application/TodoItems/Commands/UserCommands/CreateUserCommandValidator.cs
@@ -8,7 +8,7 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
     {
         RuleFor(v => v.Age)
             .GreaterThan(14)
-            .LessThan(100);
+            .LessThan(150);
         RuleFor(v => v.Username)
             .NotEmpty()
             .MaximumLength(20);
