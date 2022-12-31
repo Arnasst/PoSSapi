f9e9296a831385af0cfc1d4ef9eed2e75de86b33
geigosam <56157793+geigosam@users.noreply.github.com>
2022-12-31 18:54:31 +0200

    Renamed column "CompletionTime" to "TimeWhenCompleted" in table "Payments" to conform with specifications.

---
+++ PoSSapi/src/Infrastructure/Migrations/20221231165321_ChangePaymentTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoSSapi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompletionTime",
+                table: "Payments",
+                newName: "TimeWhenCompleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeWhenCompleted",
+                table: "Payments",
+                newName: "CompletionTime");
+        }
+    }
+}
