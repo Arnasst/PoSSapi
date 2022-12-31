08d8a8239864c46f5c023e8bb71eac98454e614a
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2022-12-31 17:33:24 +0200

    Remove unused services and dependency injection

--- PoSSapi/src/Application/Common/Behaviours/LoggingBehaviour.cs
+++ PoSSapi/src/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -1,5 +1,4 @@
-﻿using PoSSapi.Application.Common.Interfaces;
-using MediatR.Pipeline;
+﻿using MediatR.Pipeline;
 using Microsoft.Extensions.Logging;
 
 namespace PoSSapi.Application.Common.Behaviours;
@@ -7,28 +6,19 @@ namespace PoSSapi.Application.Common.Behaviours;
 public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
 {
     private readonly ILogger _logger;
-    private readonly ICurrentUserService _currentUserService;
-    private readonly IIdentityService _identityService;
 
-    public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService, IIdentityService identityService)
+    public LoggingBehaviour(ILogger<TRequest> logger)
     {
         _logger = logger;
-        _currentUserService = currentUserService;
-        _identityService = identityService;
     }
 
-    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    public Task Process(TRequest request, CancellationToken cancellationToken)
     {
         var requestName = typeof(TRequest).Name;
-        var userId = _currentUserService.UserId ?? string.Empty;
-        string userName = string.Empty;
 
-        if (!string.IsNullOrEmpty(userId))
-        {
-            userName = await _identityService.GetUserNameAsync(userId);
-        }
-
-        _logger.LogInformation("PoSSapi Request: {Name} {@UserId} {@UserName} {@Request}",
-            requestName, userId, userName, request);
+        _logger.LogInformation("PoSSapi Request: {Name} {@Request}",
+            requestName, request);
+        
+        return Task.CompletedTask;
     }
 }
